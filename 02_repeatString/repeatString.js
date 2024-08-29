0cb3ae6dfb8a8f6fe808a8f8220a879985d8b0bf
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-08-29 12:49:20 -0400

    finish exercise 02-repeat-string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = (string, num) => {
+  if (num < 0) {
+    return 'ERROR';
+  }
+  let repeatedString = "";
+  for (let i = 0; i < num; i++) {
+    repeatedString = repeatedString.concat(string);
+  }
+  return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;

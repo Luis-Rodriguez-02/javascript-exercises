f81e17694e32f4b50acec3d4d63ea50c66a89366
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-08-30 12:23:29 -0400

    finish exercise 04-remove-from-array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+  const newArray = []
 
+  array.forEach(argument => {
+    if (!args.includes(argument)) {
+      newArray.push(argument)
+    }
+  })
+  
+  return newArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;

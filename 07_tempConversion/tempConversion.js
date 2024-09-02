b3a2944882d14a06a204e846a369e3b6e03e99d9
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-09-01 20:15:04 -0400

    finish exercise 07-temp-conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  let tempCelsius = ((temperatureFahrenheit - 32) * 5 / 9);
+  return Math.round(tempCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  let tempFahrenheit = ((temperatureCelsius * (9 / 5) + 32));
+  return Math.round(tempFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line

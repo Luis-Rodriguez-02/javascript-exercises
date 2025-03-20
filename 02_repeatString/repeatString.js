cfdf028e6dee71434dee640b8027016efb02ac8d
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-03-19 20:38:50 -0400

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, numTimes) {
+  //create var to hold newString
+  if (numTimes < 0) return "ERROR";
+  let newString = "";
+  // loop until provided num times
+  for (let i = 0; i < numTimes; i++) {
+    newString = newString.concat(str);
+  }
 
+  return newString;
 };
 
 // Do not edit below this line

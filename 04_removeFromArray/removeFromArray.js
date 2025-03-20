cbc0da6cb63b5069817ad6fa4b4867a6ecf40c2e
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-03-20 19:20:49 -0400

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toBeDeleted) {
+  return arr.filter((item) => {
+    let keepItem = true;
+    for (const itemToDelete of toBeDeleted) {
+      if (item === itemToDelete) {
+        keepItem = false;
+        break;
+      }
+    }
+    return keepItem;
+  });
 };
+
+/*
+Another correct solution
+Traverse backwards to avoid skipping elements after splice shifts the index values
+
+let arr = [1, 2, 2, 3];
+for (let i = arr.length - 1; i >= 0; i--) {
+  if (arr[i] === 2) {
+    arr.splice(i, 1);
+  }
+}
+console.log(arr); // Correct output: [1, 3]
+
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;

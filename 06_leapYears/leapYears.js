9ee0c9ea92e2f62c5bfed1ef0397f4184d5171c6
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-03-20 20:34:36 -0400

    Finish exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (year % 4 == 0) {
+    // if divisble by 4
+    if (year % 100 === 0) {
+      // if divisible by 100 must be by 400 to be leap
+      return year % 400 === 0;
+    }
 
+    // if not divisible by 100 but by 4, must be leap yr
+    return true;
+  } else {
+    // not divisible by 4, not leap yr
+    return false;
+  }
 };
+
+// msut be divisible by 4
+
+// if divisible by 100 , must be divisible by 400
 
 // Do not edit below this line
 module.exports = leapYears;

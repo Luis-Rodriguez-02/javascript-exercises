8bfadb11385a8ebca025072945a4b0a222c00203
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-04-16 19:15:52 -0400

    Finish findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
 
+  const oldestPerson = people.reduce((oldestPerson, currPerson) => {
+    let newAge = currPerson.yearOfDeath
+      ? currPerson.yearOfDeath - currPerson.yearOfBirth
+      : currentYear - currPerson.yearOfBirth;
+    let oldAge = oldestPerson.yearOfDeath
+      ? oldestPerson.yearOfDeath - oldestPerson.yearOfBirth
+      : currentYear - oldestPerson.yearOfBirth;
+
+    oldestPerson = newAge > oldAge ? currPerson : oldestPerson;
+
+    return oldestPerson;
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line

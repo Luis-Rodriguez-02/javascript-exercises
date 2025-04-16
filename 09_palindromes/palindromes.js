4f794398e778801dac814566ace01777d0390fe5
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-04-16 16:08:18 -0400

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str1) {
+  const alphaNumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const onlyAlpha = str1
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphaNumerical.includes(char))
+    .join("");
+
+  const reversedOnlyAlpha = onlyAlpha.split("").reverse().join("");
+
+  return onlyAlpha == reversedOnlyAlpha;
 };
+
+palindromes("racecar!");
 
 // Do not edit below this line
 module.exports = palindromes;

8858674109fb7617a1223cb6be3d35f8ccd02d8d
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-04-16 15:44:37 -0400

    Finish calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const sum = arr.reduce((sum, num) => {
+    return sum + num;
+  }, 0);
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  const prod = arr.reduce((product, curr) => {
+    return product * curr;
+  }, 1);
 
+  return prod;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let result = 1;
+  for (let i = b; i > 0; i--) {
+    result *= a;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+
+  if (num == 0) return 1;
+
+  for (let i = num; i > 1; num--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
